52c2c020e7817d0ed7a76b1ac81e71ac9fb4e25d
alanpcao@gmail.com <alanpcao@gmail.com>
2020-02-20 23:16:22 -0800

    Initial commit

---
+++ CodePathParstagram/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CodePathParstagram
+//
+//  Created by Alan Cao on 2/20/20.
+//  Copyright © 2020 Alan Cao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-CodePathParstagram/Pods-CodePathParstagram-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CodePathParstagram : NSObject
+@end
+@implementation PodsDummy_Pods_CodePathParstagram
+@end
